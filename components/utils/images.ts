bd56fb6521accdd5e4172270b332c9d4c5f7808c
armazempreta13 <armazempreta13@gmail.com>
2025-11-23 17:25:09 -0300

    Update images.ts

--- components/utils/images.ts
+++ components/utils/images.ts
@@ -167,8 +167,8 @@ export const IMAGES = {
         philippe: 'https://i.imgur.com/TNMBi27t.jpeg',
         guilherme: 'https://i.imgur.com/uGFGBTY.jpeg',
         marcelo: 'https://i.imgur.com/A7DlGJB.jpeg',
-        func6: 'https://i.imgur.com/T6EiYQ9.jpeg',
-        func7: 'https://i.imgur.com/vHq4Q8Ct.jpeg',
+        kaua: 'https://i.imgur.com/T6EiYQ9.jpeg',
+        higor: 'https://i.imgur.com/xCFvazu.jpeg',
         func8: 'https://i.imgur.com/Oy9Xq2jt.jpeg',
         cta: {
             background: 'https://i.imgur.com/0rQ5ev1.jpeg' // Reuso da imagem de Serviços
